69f971d50dbec68519fd1c7e801eeea303a53451
Vishal <vishal@datarpm.com>
2018-01-08 19:15:34 +0530

    js client for sigma-events

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+var pkgjson = require('./package.json');
+
+var config = {
+  pkg: pkgjson,
+  app: 'js',
+  dist: 'dist'
+}
+
+module.exports = function (grunt) {
+  // Configuration
+  grunt.initConfig({
+    config: config,
+    pkg: config.pkg,
+    bower: grunt.file.readJSON('./.bowerrc'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> lib - v<%= pkg.version %> -' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      dist: {
+        files: {
+          '<%= config.dist %>/js/event-engine.js': [
+            '<%= config.app %>/**/*.js',
+            '<%= bower.directory %>/reconnectingWebsocket/reconnecting-websocket.js',
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', [
+    'uglify'
+  ]);
+};
